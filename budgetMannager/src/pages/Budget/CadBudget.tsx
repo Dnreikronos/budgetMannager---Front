a6bc88670ba7cd52d0582d615f05df73440356ee
Dnreikronos <dnreikronos2@gmail.com>
2024-12-12 20:16:07 -0400

    feat: create necessary hooks for handle the integrations with backend

--- budgetMannager/src/pages/Budget/CadBudget.tsx
+++ budgetMannager/src/pages/Budget/CadBudget.tsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import Sidebar from "../../components/ui/sidebar.tsx";
 
 
 
 const CadBudget = () => {
-  return (
-  <>
-    <Sidebar />
-  </>
-  );
+	const [value, setValue] = useState<number | string>("");
+	const [currency, setCurrency] = useState<string>("");
+	const [start, setStart] = useState<string>("");
+	const [end, setEnd] = useState<string>("");
+	const [error, setError] = useState<string | null>(null);
+	const [loading, setLoading] = useState<boolean>(false);
+
+	return (
+		<>
+			<Sidebar />
+		</>
+	);
 };
 
 export default CadBudget;
