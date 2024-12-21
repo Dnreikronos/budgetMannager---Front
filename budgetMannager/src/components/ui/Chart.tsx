a686f8fa94486a5bede7306eed3ee156ac333e2b
Dnreikronos <dnreikronos2@gmail.com>
2024-12-21 09:07:55 -0400

    feat: register required modules

---
+++ budgetMannager/src/components/ui/Chart.tsx
@@ -0,0 +1,15 @@
+import {Line, Bar, Pie} from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  BarElement,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Card, CardContent, CardHeader, CardTitle } from "./card";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarElement, ArcElement, Tooltip, Legend);
