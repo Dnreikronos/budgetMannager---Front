1e9a817d96aff7f2fdc1a016d9a3ee7c0ddc9687
Dnreikronos <dnreikronos2@gmail.com>
2024-12-21 09:09:21 -0400

    feat: create ChartProps type

--- budgetMannager/src/components/ui/Chart.tsx
+++ budgetMannager/src/components/ui/Chart.tsx
@@ -1,15 +1,24 @@
-import {Line, Bar, Pie} from "react-chartjs-2";
+import { Line, Bar, Pie } from "react-chartjs-2";
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  BarElement,
-  ArcElement,
-  Tooltip,
-  Legend,
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	BarElement,
+	ArcElement,
+	Tooltip,
+	Legend,
 } from "chart.js";
 import { Card, CardContent, CardHeader, CardTitle } from "./card";
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarElement, ArcElement, Tooltip, Legend);
+
+type ChartProps = {
+	type: "line" | "bar" | "pie";
+	data: any;
+	option?: any;
+	title?: string;
+};
+
+
