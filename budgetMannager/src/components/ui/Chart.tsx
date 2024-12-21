3ea8d464ac85e7766e97b394c5fc0af5b82c981f
Dnreikronos <dnreikronos2@gmail.com>
2024-12-21 09:10:28 -0400

    style: create the stylesheet for the chart component

--- budgetMannager/src/components/ui/Chart.tsx
+++ budgetMannager/src/components/ui/Chart.tsx
@@ -17,8 +17,24 @@ ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, BarEleme
 type ChartProps = {
 	type: "line" | "bar" | "pie";
 	data: any;
-	option?: any;
+	options?: any;
 	title?: string;
 };
 
+const Chart: React.FC<ChartProps> = ({ type, data, options, title }) => {
+  const ChartComponent = type === "line" ? Line : type === "bar" ? Bar : Pie;
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title || "Chart"}</CardTitle>
+      </CardHeader>
+      <CardContent className="p-4">
+        <ChartComponent data={data} options={options} />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Chart;
 
